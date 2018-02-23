2df397e906e0d5be669026f5f7acfdc4f89ef652
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-02-23 20:14:21 -0300

    haskell start

---
+++ definitions.hs
@@ -0,0 +1,15 @@
+three :: Integer -> Integer
+three x = 3
+
+infinity :: Integer
+infinity = infinity + 1
+
+double :: Integer -> Integer
+double 2 = 2
+double x = x + x
+
+
+multiply :: (Integer, Integer) -> Integer
+multiply (x,y) = x * y
+
+
