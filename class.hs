e47a351ddf800fba2d89a0d942f2b57ad4718b49
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-02-28 19:14:13 -0300

    playing with types

--- class.hs
+++ class.hs
@@ -1,5 +1,6 @@
 module Curry where
 import Prelude hiding ( curry )
+
 -- calcular hipotenusa
 pyth :: Double -> Double -> Double
 pyth x y = sqrt (x ** 2 + y ** 2)
@@ -10,5 +11,21 @@ pyth' (x,y) = sqrt ( x ** 2 + y ** 2 )
 myCurry :: ((Double, Double) -> Double) -> (Double -> Double -> Double)
 myCurry f x y = f (x,y)
 
-unCarry :: (Double -> Double -> Double) -> ((Double, Double) -> Double)
-unCarry f (x,y) = f x y
+unCurry :: (Double -> Double -> Double) -> ((Double, Double) -> Double)
+unCurry f (x,y) = f x y
+
+-- Playing with types
+
+curry2 :: ( (a,b) -> c ) -> a -> ( b -> c )
+curry2 f x = g
+	where
+	  g u = f (x,u)
+
+curry3 :: ( (a,b) -> c ) -> a -> b -> c
+curry3 f x y = f(x,y)
+
+uncurry2' :: (a -> b -> c) -> (a,b) -> c
+uncurry2' f w = f(fst w)(snd w)
+
+uncurry2 :: (a -> b -> c ) -> (a,b) -> c
+uncurry2 f (x,y) = f x y
