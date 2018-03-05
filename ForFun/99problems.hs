605741852b12ebc72ecff8599d3f00594f5fef73
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-03-05 16:36:36 -0300

    new problems solved

--- ForFun/99problems.hs
+++ ForFun/99problems.hs
@@ -17,3 +17,12 @@ myButLast :: [a] -> a
 myButLast [] = undefined
 myButLast (x:[y]) = x
 myButLast (_:xs) = myButLast xs
+
+-- Problem 3
+-- (*) Find the K'th element of a list. The first element in the list is number 1. 
+elementAt :: [a] -> Integer -> a
+elementAt [] y = undefined
+elementAt (x:_) 1 = x
+elementAt (_:xs) y
+	| y < 1 = undefined
+	| otherwise = elementAt xs (y-1)
